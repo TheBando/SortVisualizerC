ea72dc3565b48d2c84324ee5399ed8d051ff443d
TheBando <87739891+TheBando@users.noreply.github.com>
2023-08-08 15:14:36 -0400

    Update sort.c
    updated for radix sort

--- sort.c
+++ sort.c
@@ -43,7 +43,63 @@ void quick_sort_with_ptr(int * array, int low, int high)
         quick_sort_with_ptr(array, index + 1, high);
     }
 }
-void quick_sort(int array[], int low, int high)
-{
 
+
+/***********************************************************/
+int get_max(int * array, int size)
+{
+    int max = 0;
+    for (int i = 0; i < size; i ++)
+    {
+        if (*(array + i) > max)
+            max = *(array + i);
+
+    }
+    return max;
+}
+
+void counting_sort(int * array, int size, int place)
+{
+    int * out = malloc(sizeof *out * size);
+    int max = (*(array) / place) % 10;
+
+    for (int i = 1; i < size; i++)
+    {
+        if (((*(array + i) / place) % 10) > max)
+            max = *(array + i);
+    }
+    int * count = malloc(sizeof *count * (max + 1));
+
+    for (int i = 0; i < max; i++)
+    {
+        *(count + i) = 0;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        *(count + ((*(array + i) / place) % 10)) += 1;
+    }
+    for (int i = 1; i < 10; i++)
+    {
+        *(count + i) += *(count + (i - 1));
+    }
+    for (int i = size - 1; i >= 0; i--)
+    {
+        *(out + ((*(count + (*(array +i) / place) % 10) -1 ))) = *(array + i);
+        *(count + ((*(array + i) / place) % 10)) -= 1;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        *(array + i) = *(out + i);
+    }
+}
+
+void radix_sort(int * array, int size)
+{
+    int max = get_max(array, size);
+    refresh(array);
+    for (int place = 1; max / place > 0; place *= 10)
+    {
+        counting_sort(array, size, place);
+        refresh(array);
+    }
 }
